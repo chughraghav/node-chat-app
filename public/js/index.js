5770f00fbd486c6d46f264cdfca540177e1e8a3f
Raghav <chughraghav@gmail.com>
2019-04-20 16:45:04 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect',function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from : 'Clint',
-    text : 'Send help'
-  });
 });
 
 socket.on('newMessage', function(message) {
